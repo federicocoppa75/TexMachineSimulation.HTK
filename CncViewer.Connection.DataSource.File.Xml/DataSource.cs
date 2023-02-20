df9dc15c9c2e3ad17d15f79d6648c2b7b4fba7c5
Federico Coppa <federicocoppa75@gmail.com>
2023-02-20 09:42:51 +0100

    dev - implementation environment extension for connection variables file

--- CncViewer.Connection.DataSource.File.Xml/DataSource.cs
+++ CncViewer.Connection.DataSource.File.Xml/DataSource.cs
@@ -8,33 +8,66 @@ using CVM = CncViewer.Models;
 using CncViewer.Models;
 using CVCI = CncViewer.Connection.Interfaces;
 using Machine.ViewModels.UI;
+using MVMUIDSF = Machine.ViewModels.UI.DataSource.File;
+using System.IO;
+using System.Linq;
 
 namespace CncViewer.Connection.DataSource.File.Xml
 {
-    public class DataSource : CVCI.IDataSource
+    public class DataSource : CVCI.IDataSource, MVMUIDSF.IDataSourceExtension
     {
+        private string _lastVariableFile;
+        private const string _extension = "xCncLink";
+
         private ICommand _openCommand;
         public ICommand OpenCommand => _openCommand ?? (_openCommand = new RelayCommand(() => OpenCommandImpl()));
+
+        public IEnumerable<string> Files
+        {
+            get
+            {
+                var list = new List<string>();
+
+                if (!string.IsNullOrEmpty(_lastVariableFile))
+                {
+                    list.Add(_lastVariableFile);
+                }
+
+                return list;
+            }
+        }
+
+        public DataSource()
+        {
+            MVMIoc.SimpleIoc<MVMUIDSF.IDataSourceExtension>.Register(this);
+        }
 
         private void OpenCommandImpl()
         {
             var dlg = MVMIoc.SimpleIoc<IFileDialog>.GetInstance("OpenFile");
 
             dlg.AddExtension = true;
-            dlg.DefaultExt = "xCncLink";
-            dlg.Filter = "Links to variable configuration |*.xCncLink";
+            dlg.DefaultExt = _extension;
+            dlg.Filter = $"Links to variable configuration |*.{_extension}";
 
             var b = dlg.ShowDialog();
 
             if (b.HasValue && b.Value)
             {
-                var data = LoadConfiguration(dlg.FileName);
-
-                if(data != null) 
-                {
-                    DeployVariable(data);
-                }
+                LoadVariables(dlg.FileName);
             }
+        }
+
+        private void LoadVariables(string fileName)
+        {
+            var data = LoadConfiguration(fileName);
+
+            if (data != null)
+            {
+                DeployVariable(data);
+            }
+
+            _lastVariableFile = fileName;
         }
 
         private void DeployVariable(ConnectionData data)
@@ -62,6 +95,24 @@ namespace CncViewer.Connection.DataSource.File.Xml
             {
                 return (CVM.ConnectionData)serializer.Deserialize(reader);
             }
+        }
+
+        public bool OnEnvironmentLoaded(string extractionPath)
+        {
+            var dir = new DirectoryInfo(extractionPath);
+
+            if(dir.Exists)
+            {
+                var file = dir.GetFiles($"*.{_extension}").FirstOrDefault();
+                
+                if(file != null) 
+                { 
+                    LoadVariables(file.FullName);
+                }
+            }
+
+
+            return true;
         }
     }
 }
