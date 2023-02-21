e9a2999ca0db56c3e2bebb015eb33d40ae1f4452
Federico Coppa <federicocoppa75@gmail.com>
2023-02-21 13:38:26 +0100

    dev - change view of flag and out variables value

---
+++ CncViewer.Connection.Views/Converters/BoolToCharConverter.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Views.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CncViewer.Connection.Views.Converters
+{
+    internal class BoolToCharConverter : BoolToTypeConverter<char>
+    {
+    }
+}
---
+++ CncViewer.Connection.Views/Selectors/VariableValueTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using CncViewer.Connection.Interfaces;
+using CncViewer.Connection.Interfaces.Enums;
+using CncViewer.Connection.Interfaces.Inputs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CncViewer.Connection.Views.Selectors
+{
+    internal class VariableValueTemplateSelector : TemplateSelector<VariableType>
+    {
+        protected override bool TryGetPropertyToCompare(object item, out VariableType value)
+        {
+            bool result = false;
+
+            if (item is IVariable v)
+            {
+                value = v.VariableType;
+                result = true;
+            }
+            else
+            {
+                value = default;
+            }
+
+            return result;
+        }
+    }
+
+    class VariableValueTemplateSelectorItem : TemplateSelectorItem<VariableType>
+    {
+    }
+}
